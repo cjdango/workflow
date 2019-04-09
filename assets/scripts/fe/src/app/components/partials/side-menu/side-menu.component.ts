fadfc761909664d0553702ec623bc35ae95237d0
Earvin Gemenez <earvin.gemenez@gmail.com>
2019-04-09 10:13:01 +0800

    fix the nav active states

--- assets/scripts/fe/src/app/components/partials/side-menu/side-menu.component.ts
+++ assets/scripts/fe/src/app/components/partials/side-menu/side-menu.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { StateService } from '@uirouter/angular';
+
 
 @Component({
   selector: 'app-side-menu',
@@ -7,9 +9,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SideMenuComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private state: StateService
+  ) { }
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
 }
