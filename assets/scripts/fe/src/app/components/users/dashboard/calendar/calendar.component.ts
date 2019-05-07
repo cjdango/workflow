77ec57fd32f07566a5e93ff1a59d032288366397
cjdango <cjdango@gmail.com>
2019-05-07 10:42:30 +0800

    Refactor dashboard calendar component.

--- assets/scripts/fe/src/app/components/users/dashboard/calendar/calendar.component.ts
+++ assets/scripts/fe/src/app/components/users/dashboard/calendar/calendar.component.ts
@@ -10,108 +10,130 @@ import { ServerService } from 'src/app/commons/services/auth/server.service';
   styleUrls: ['./calendar.component.css']
 })
 export class CalendarComponent implements OnInit {
-  isCollapsed = true;
-
-  month : number;
-  year  : number;
-  now   : Date;
+  isCollapsed : boolean;
+  month       : number;
+  year        : number;
+  now         : Date;
 
   constructor(
     private server: ServerService
   ) {
-    this.now    = this.server.now;
-    this.month  = this.now.getMonth();
-    this.year   = this.now.getFullYear();
-
-    console.log(this.weeks);
-   }
+    this.isCollapsed = true;
+    this.now         = this.server.now;
+    this.month       = this.now.getMonth();
+    this.year        = this.now.getFullYear();
+  }
 
   ngOnInit() {
   }
 
-  /**
-   * Get today's date from `ServerService`.
-   */
   get today(): number {
     return this.server.now.getDate();
   }
 
-  get prevMonthLastDate() {
+  get prevMonthLastDate(): Date {
     return new Date(this.year, this.month, 0);
   }
 
-  get prevMonthLastDateWeekIdx() {
-    return this.prevMonthLastDate.getDate();
-  }
-
-  get currMonthFirstDate() {
+  get currMonthFirstDate(): Date {
     return new Date(this.year, this.month, 1);
   }
 
-  get currMonthFirstDateWeekIdx() {
-    return this.currMonthFirstDate.getDay();
-  }
-
-  get currMonthLastDate() {
+  get currMonthLastDate(): Date {
     return new Date(this.year, this.month + 1, 0);
   }
 
-  get currMonthLastDateWeekIdx() {
-    return this.currMonthLastDate.getDay();
-  }
+  /**
+   * Get current month's firstweek including prev month's idle dates.
+   */
+  get firstWeek(): Array<Date> {
+    const currMonthFirstDateWeekIdx = this.currMonthFirstDate.getDay();
+    const prevMonth                 = this.month - 1;
+    const prevMonthDatesCount       = currMonthFirstDateWeekIdx;
+    const prevMonthDates            = [];
 
-  get nextMonthFirstDate() {
-    return new Date(this.year, this.month + 1, 1);
-  }
-
-  get nextMonthFirstDateWeekIdx() {
-    return this.nextMonthFirstDate.getDay();
-  }
-
-  get firstWeek() {
-    const prevMonthDates = [];
-
-    if (this.currMonthFirstDateWeekIdx !== 0) {
-      _.times(this.currMonthFirstDateWeekIdx, (idx) => {
-        prevMonthDates.push(new Date(this.year, this.month - 1, this.prevMonthLastDate.getDate() - idx ));
+    // Check if the current month's first date is not monday
+    // If true, push prev month dates to prevMonthDates array.
+    if (currMonthFirstDateWeekIdx !== 0) {
+      _.times(prevMonthDatesCount, (idx) => {
+        const prevMonthDate = this.prevMonthLastDate.getDate() - idx;
+        prevMonthDates.push(new Date(this.year, prevMonth, prevMonthDate));
       });
+
+      // Reverse the array since we started pushing from
+      // prevMonthLastDate backwards.
       prevMonthDates.reverse();
     }
 
-    return [...prevMonthDates, ...this.monthDates].slice(0, 7);
+    // Combine prev month dates and current month dates
+    // then return only the first 7 dates as the first week.
+    return [...prevMonthDates, ...this.currMonthDates].slice(0, 7);
   }
 
-  get lastWeek() {
-    const nextMonthDates = [];
+  /**
+   * Get current month's lastweek including next month's idle dates.
+   */
+  get lastWeek(): Array<Date> {
+    const currMonthLastDateWeekIdx = this.currMonthLastDate.getDay();
+    const nextMonth                = this.month + 1;
+    const nextMonthDatesCount      = 6 - currMonthLastDateWeekIdx;
+    const nextMonthDates           = [];
 
-    if (this.currMonthLastDateWeekIdx !== 6) {
-      _.times(6 - this.currMonthLastDateWeekIdx, (idx) => {
-        nextMonthDates.push(new Date(this.year, this.month + 1, idx + 1 ));
+    // Check if the current month's last date is not saturday
+    // If true, push next month dates to nextMonthDates array.
+    if (currMonthLastDateWeekIdx !== 6) {
+      _.times(nextMonthDatesCount, (idx) => {
+        const nextMonthDate = idx + 1;
+        nextMonthDates.push(new Date(this.year, nextMonth, nextMonthDate));
       });
     }
 
-    return [...this.monthDates, ...nextMonthDates].slice(this.monthDates.length - 6);
+    // Combine next month dates and current month dates
+    // then return only the last 7 dates as the last week.
+    return [...this.currMonthDates, ...nextMonthDates].slice(this.currMonthDates.length - 6);
   }
 
-  get monthDates() {
-    const currMonthDates  = [];
+  /**
+   * Get current month's dates not including idle dates.
+   */
+  get currMonthDates(): Array<Date> {
+    const datesCount = this.currMonthLastDate.getDate();
+    const dates      = [];
 
-    _.times(this.currMonthLastDate.getDate(), (idx) => {
-      currMonthDates.push(new Date(this.year, this.month, idx + 1));
+    // Push current month dates into dates array.
+    _.times(datesCount, (idx) => {
+      const date = idx + 1;
+      dates.push(new Date(this.year, this.month, date));
     });
 
-    return currMonthDates;
+    return dates;
   }
 
-  get weeks() {
+  /**
+   * Get currnent month's dates divided into several weeks
+   * 7 dates per week.
+   */
+  get weeks(): Array<Date> {
     const prevMonthShownDate = this.firstWeek.filter(date => date.getMonth() !== this.month);
     const nextMonthShownDate = this.lastWeek.filter(date => date.getMonth() !== this.month);
 
     return _.chunk([
       ...prevMonthShownDate,
-      ...this.monthDates,
+      ...this.currMonthDates,
       ...nextMonthShownDate
     ], 7);
+  }
+
+  /**
+   * @summary Generate object of css classes for a `date`
+   * @param date Insatance of Date;
+   * @description Can be use for ngClass directive
+   */
+  getDateClass(date: Date): {'idleDay': boolean, 'today': boolean} {
+    return {
+      'idleDay': date.getMonth() !== this.month,
+      'today': date.getDate() === this.today
+    };
   }
 
 }
