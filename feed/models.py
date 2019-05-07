e40b07affee5b0caf96ee8d2d6783ae97334d2f6
cjdango <cjdango@gmail.com>
2019-05-07 16:21:56 +0800

    Add frequency field in Feed model.

--- feed/models.py
+++ feed/models.py
@@ -1,10 +1,23 @@
 from django.db import models
+from django.utils import timezone
+
 from users.models import User
 
 
 class Event(models.Model):
     """ event schedules
     """
+    REPEAT_NONE = 0
+    REPEAT_DAILY = 1
+    REPEAT_WEEKLY = 2
+    REPEAT_MONTHLY = 3
+    RECURRENCE_CHOICES = (
+        (REPEAT_NONE, 'None'),
+        (REPEAT_DAILY, 'Daily'),
+        (REPEAT_WEEKLY, 'Weekly'),
+        (REPEAT_MONTHLY, 'Monthly')
+    )
+
     title = models.CharField(max_length=200)
     content = models.TextField()
 
@@ -14,6 +27,8 @@ class Event(models.Model):
     event_date = models.DateField(null=True, blank=True)
     start_time = models.TimeField(null=True, blank=True)
     end_time = models.TimeField(null=True, blank=True)
+
+    frequency = models.IntegerField(choices=RECURRENCE_CHOICES, default=REPEAT_NONE)
 
     date_created = models.DateTimeField(auto_now_add=True)
     date_updated = models.DateField(auto_now=True)
