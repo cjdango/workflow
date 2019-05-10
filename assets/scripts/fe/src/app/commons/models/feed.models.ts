0964ca4e77c0b42a04456dafaf8784fda01c5db5
cjdango <cjdango@gmail.com>
2019-05-10 13:16:35 +0800

    Add Event frontend model.

--- assets/scripts/fe/src/app/commons/models/feed.models.ts
+++ assets/scripts/fe/src/app/commons/models/feed.models.ts
@@ -6,3 +6,18 @@ export class Feed {
     Object.assign(this, data);
   }
 }
+
+export class Event {
+  title         : string = null;
+  content       : string = null;
+  participants  : Array<object> = null;
+  event_date    : Date = null;
+  start_time    : Date = null;
+  end_time      : Date = null;
+  frequency     : string = null;
+
+
+  constructor(data = {}) {
+    Object.assign(this, data);
+  }
+}
