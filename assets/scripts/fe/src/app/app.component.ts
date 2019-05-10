37ff887b3ce33831bb4cb953b58407740214a4b5
cjdango <cjdango@gmail.com>
2019-05-10 20:57:11 +0800

    Fetch all users before the app loads.

--- assets/scripts/fe/src/app/app.component.ts
+++ assets/scripts/fe/src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { NavService } from './commons/services/utils/nav.service';
 import { ServerService } from './commons/services/auth/server.service';
+import { UsersService } from './commons/services/users/users.service';
+import { User } from './commons/models/user.models';
 
 
 @Component({
@@ -12,9 +14,16 @@ export class AppComponent {
   
   constructor(
     private nav    : NavService,
-    private server : ServerService
+    private server : ServerService,
+    private usersService: UsersService
   ) {
     // load server date time
     server.syncServerDT();
+
+    // fetch all users and store to `UsersService.allUSers`
+    usersService.fetchUsers()
+      .then((data: Array<User>) => {
+        this.usersService.allUsers = data;
+      });
   }
 }
