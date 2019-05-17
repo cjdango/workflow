ad96c25be09394b9f90ca50747df21c7c503f95b
cjdango <cjdango@gmail.com>
2019-05-17 15:34:27 +0800

    Update Event model.

--- assets/scripts/fe/src/app/commons/models/feed.models.ts
+++ assets/scripts/fe/src/app/commons/models/feed.models.ts
@@ -1,3 +1,5 @@
+import { ShortUser } from "./user.models";
+
 export class Feed {
   instance : Object = {};
   instance_type : string = null;
@@ -8,13 +10,22 @@ export class Feed {
 }
 
 export class Event {
-  title         : string = null;
-  content       : string = null;
-  participants  : Array<object> = null;
-  event_date    : string = null;
-  start_time    : string = null;
-  end_time      : string = null;
-  frequency     : string = null;
+  title                     : string = null;
+  content                   : string = null;
+  event_date                : string = null;
+  start_time                : string = null;
+  end_time                  : string = null;
+  frequency                 : string = null;
+  participants_id           : Array<string> = null;
+  readonly id               : string = null;
+  readonly participants     : Array<ShortUser> = null;
+  readonly organizer        : any    = null;
+  readonly freq_day         : string = null;
+  readonly freq_hr          : string = null;
+  readonly freq_min         : string = null;
+  readonly freq_mo          : string = null;
+  readonly freq_week_idx    : string = null;
+  readonly date_created     : string = null;
 
 
   constructor(data = {}) {
